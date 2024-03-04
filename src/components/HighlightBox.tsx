616dab57c82eee69d0f78ee86ce55b0bef8a1f57
Mathias Vogel <mathias.vo@outlook.com>
2024-03-05 00:57:47 +0100

    Add docu

--- src/components/HighlightBox.tsx
+++ src/components/HighlightBox.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const HighlightBox = ({ children }) => (
-  <div style={{ backgroundColor: '#FFF9C4', padding: '3px', marginBottom: '10px', border: '2px solid #ccc', borderRadius: '5px', textAlign: 'center' }}>
+  <div style={{ backgroundColor: '#FFF9C4', padding: '3px', margin: '0 auto 20px auto', border: '2px solid #ccc', borderRadius: '5px', textAlign: 'center', width:'95%' }}>
     {children}
   </div>
 );
